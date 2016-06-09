409026768f0c8ae8943eb88081a269a64fb9caac
paulkenrick <pkenrick@gmail.com>
2016-06-09 11:29:23 +0100

    Tidying up generally

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,8 @@
 feature 'Shows opponent\'s HP' do
+  
   scenario 'initiating battle' do
     sign_in_and_play
-    expect(page).to have_content 'Enzo: 60HP'
+    expect(page).to have_content Player::DEFAULT_HIT_POINTS
   end
+  
 end
