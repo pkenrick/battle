de241adcc2570597d0182200974396c10c2c3b53
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-06-09 13:03:05 +0100

    Add attack hit points function

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -16,15 +16,17 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.player_name
-    @player_2_name = $player_2.player_name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
   get '/attack' do
-  	@player_1_name = $player_1.player_name
-    @player_2_name = $player_2.player_name
+  	@player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
+
   end
 
   run! if app_file == $0
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -9,7 +9,7 @@ feature "attacking" do
 		sign_in_and_play
 		click_button 'Attack'
 		click_button 'OK'
-		expect(page).not_to have_content "Dave: 80HP"
-		expect(page).to have_content "Dave: 70HP"
+		expect(page).not_to have_content "Harry: 80HP"
+		expect(page).to have_content "Harry: 70HP"
 	end
 end
